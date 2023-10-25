e78bdc458bd807ddd7cf1b8b7e1178f399f5452c
Matheus Moura <matheusmoura78@hotmail.com>
2023-10-25 11:40:11 -0300

    WebSocket Server

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ server: http });
+
+// wss.on("connection", (socket) => {
+//     socket.on("disconnect", () => {
+//         console.log("Desconectado: " + socket.id);
+//     });
+
+//     socket.on("getRecebimento", (data) => {
+//         console.log(data.recebimentos);
+//     });
+
+// })
+
+wss.on('connection', (socket) => {
+    console.log("A new client Connected!");
+
+    socket.send("Welcome New Client!");
+    socket.on("message", (message) => {
+        console.log("Received: %s", message);
+        socket.send("Got your msg its: " + message);
+    });
+
+});
+
+http.listen(3030, () => {
+    console.log("App rodando!");
+});
